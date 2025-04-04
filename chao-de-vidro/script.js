6fe6478f1994f69ad22f34163fa7adee1efa7ac7
Victor Portugal <victorportugalnobrega@gmail.com>
2025-04-04 11:33:34 -0300

    Colando o for

--- chao-de-vidro/script.js
+++ chao-de-vidro/script.js
@@ -1,21 +1,21 @@
-rodada = 1
-while (rodada <= 3) {
+ganhou = 0
+
+for(let rodada = 1; rodada <= 10; rodada++) {
     console.log(`Rodada ${rodada}`)
     
     escolhaJogador = prompt(`Nível ${rodada} - Escolha um número entre 1 e 3:`)
-    if (escolhaJogador < 1 || escolhaJogador > 3) {
+    if (escolhaJogador < 1 || escolhaJogador > 10) {
         alert("Número inválido! Escolha um número entre 1 e 3.")
         continue
     }
-    pisoQuebrado = Math.floor(Math.random() * 3) + 1
+    pisoQuebrado = Math.floor(Math.random() * 10) + 1
 
     if (escolhaJogador == pisoQuebrado) {
         alert("Você quebrou o piso! Fim de jogo!")
+        ganhou = 1
         break
     } else {
         alert(`Você não quebrou o piso! O piso quebrado estava na ponte ${pisoQuebrado}. Você passou para o próximo nível!`)
     }
-    
-    rodada++
 }
-if(rodada == 4) alert("Você venceu o jogo. Parabéns!")
+if(ganhou == 0) alert("Você venceu o jogo. Parabéns!")
